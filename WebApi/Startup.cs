d3def92f26383873469dc2c3c8a5cafc7f2ed606
filiniti <filiniti@gmail.com>
2019-12-17 20:16:00 +0200

    app

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using WebApi.BisinessLogic.Books;
 
@@ -13,6 +9,12 @@ namespace WebApi
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public readonly IConfiguration Configuration;
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -23,7 +25,7 @@ namespace WebApi
 
             services.AddDistributedRedisCache(option =>
             {
-                option.Configuration = "webredis:6379";
+                option.Configuration = Configuration.GetConnectionString("redis");
             });
         }
 
